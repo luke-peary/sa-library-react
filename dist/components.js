8f90b066d5ab86661dfeeb597c232f7c4700770d
luke-peary <luke.peary@accenture.com>
2020-09-18 13:41:26 +1000

    first commit

---
+++ dist/components.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+export const SaButton = /*@__PURE__*/ createReactComponent('sa-button');
+export const SaCopy = /*@__PURE__*/ createReactComponent('sa-copy');
+export const SaHeading = /*@__PURE__*/ createReactComponent('sa-heading');
+export const SaImage = /*@__PURE__*/ createReactComponent('sa-image');
+export const SaProductTile = /*@__PURE__*/ createReactComponent('sa-product-tile');
+export const SaTextInput = /*@__PURE__*/ createReactComponent('sa-text-input');
+export const TwgOffCanvas = /*@__PURE__*/ createReactComponent('twg-off-canvas');
+//# sourceMappingURL=components.js.map
---
+++ src/components.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from 'superawesomewc';
+
+
+
+export const SaButton = /*@__PURE__*/createReactComponent<JSX.SaButton, HTMLSaButtonElement>('sa-button');
+export const SaCopy = /*@__PURE__*/createReactComponent<JSX.SaCopy, HTMLSaCopyElement>('sa-copy');
+export const SaHeading = /*@__PURE__*/createReactComponent<JSX.SaHeading, HTMLSaHeadingElement>('sa-heading');
+export const SaImage = /*@__PURE__*/createReactComponent<JSX.SaImage, HTMLSaImageElement>('sa-image');
+export const SaProductTile = /*@__PURE__*/createReactComponent<JSX.SaProductTile, HTMLSaProductTileElement>('sa-product-tile');
+export const SaTextInput = /*@__PURE__*/createReactComponent<JSX.SaTextInput, HTMLSaTextInputElement>('sa-text-input');
+export const TwgOffCanvas = /*@__PURE__*/createReactComponent<JSX.TwgOffCanvas, HTMLTwgOffCanvasElement>('twg-off-canvas');
